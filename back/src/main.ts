5f33ce56e9adb2737149a39495bcdcf7a3ad5dd6
IIbreathII <96979747+IIbreathII@users.noreply.github.com>
2025-01-20 15:35:50 +0200

    1.7.2

--- back/src/main.ts
+++ back/src/main.ts
@@ -19,6 +19,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document); // Доступ по адресу /api
 
-  await app.listen(3000);
+  await app.listen(57806);
 }
 bootstrap();
