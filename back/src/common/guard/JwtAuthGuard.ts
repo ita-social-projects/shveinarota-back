6e2e9e853a63fdd8b57b2e01e7b4699523713ec1
IIbreathII <96979747+IIbreathII@users.noreply.github.com>
2025-02-16 08:40:32 +0200

    6.2

--- back/src/common/guard/JwtAuthGuard.ts
+++ back/src/common/guard/JwtAuthGuard.ts
@@ -8,23 +8,19 @@ export class JwtAuthGuard implements CanActivate {
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest<Request>();
-    console.log('Headers:', request.headers);
     
     // Отримуємо токен із кукі
     const authToken = request.cookies?.auth_token;
 
     if (!authToken || typeof authToken !== 'string') {
-      console.log('No token provided or invalid type');
       throw new UnauthorizedException('No token provided');
     }
 
     try {
       const payload = await this.jwtService.verifyAsync(authToken.trim());
-      console.log('Token payload:', payload);
       (request as any).user = payload;
       return true;
     } catch (error) {
-      console.log('Token verification failed:', error.message);
       throw new UnauthorizedException('Invalid token');
     }
   }
