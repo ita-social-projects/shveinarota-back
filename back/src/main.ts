5c7b2afb2e19e81481821936576a51e7b608c50e
IIbreathII <96979747+IIbreathII@users.noreply.github.com>
2025-02-17 11:34:26 +0200

    8.8.5

--- back/src/main.ts
+++ back/src/main.ts
@@ -36,7 +36,6 @@ async function bootstrap() {
   const client = configService.get<string>('CLIENT_NAME')
 
   app.enableCors({
-    origin: "https://shveinarota.vercel.app",
     credentials: true, // Разрешает куки
   });
 
