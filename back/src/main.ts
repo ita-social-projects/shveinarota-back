095bb57fae5e88306015e7dee43a36f61fa2d652
IIbreathII <96979747+IIbreathII@users.noreply.github.com>
2025-02-21 21:02:41 +0200

    ORS DISABLED

--- back/src/main.ts
+++ back/src/main.ts
@@ -36,7 +36,6 @@ async function bootstrap() {
   const client = configService.get<string>('CLIENT_NAME')
 
   app.enableCors({
-    origin: `${client}`,
     credentials: true, // Разрешает куки
   });
 
