2f72efef04e3de584f41fa468e579a1daed63237
IIbreathII <96979747+IIbreathII@users.noreply.github.com>
2025-03-09 23:46:52 +0200

    9.2.1

--- back/src/admin/team_members/team.controller.ts
+++ back/src/admin/team_members/team.controller.ts
@@ -19,13 +19,13 @@ import { ApiTags, ApiOperation, ApiResponse, ApiParam, ApiConsumes, ApiBody } fr
 import { AnyFilesInterceptor, FileInterceptor } from '@nestjs/platform-express';
 import { JwtAuthGuard } from '../../common/guard/JwtAuthGuard'; 
 
-@ApiTags('Картки')
+@ApiTags('Команда')
 @Controller(':lang/teams')
 export class TeamsController {
   constructor(private readonly TeamsService: TeamsService) {}
 
   @Get()
-  @ApiOperation({ summary: 'Отримати всі картки для вказаної мови' })
+  @ApiOperation({ summary: 'Отримати всі члени для вказаної мови' })
   @ApiParam({ name: 'lang', required: true, description: 'Мова карток (en або uk)' })
   @ApiResponse({ status: 200, description: 'Список карток успішно отримано' })
   @ApiResponse({ status: 400, description: 'Неправильний формат мови' })
@@ -34,28 +34,28 @@ export class TeamsController {
   }
 
   @Get('all')
-  @ApiOperation({ summary: 'Отримати всі картки незалежно від мови' })
+  @ApiOperation({ summary: 'Отримати всі члени незалежно від мови' })
   @ApiResponse({ status: 200, description: 'Список всіх карток отримано' })
   async getAllTeams() {
     return this.TeamsService.getAllTeams();
   }
 
   @Get(':id')
-  @ApiOperation({ summary: 'Отримати картку за ID' })
-  @ApiParam({ name: 'id', required: true, description: 'ID картки' })
+  @ApiOperation({ summary: 'Отримати члена за ID' })
+  @ApiParam({ name: 'id', required: true, description: 'ID члени' })
   @ApiResponse({ status: 200, description: 'Картка успішно отримана' })
-  @ApiResponse({ status: 404, description: 'Картку не знайдено' })
+  @ApiResponse({ status: 404, description: 'Члена команди не знайдено' })
   async getTeamById(@Param('id', ParseIntPipe) id: number) {
     return this.TeamsService.getTeamById(id);
   }
 
   @Post()
   @UseGuards(JwtAuthGuard) 
-  @ApiOperation({ summary: 'Створити нову картку' })
+  @ApiOperation({ summary: 'Створити нову члена' })
   @ApiConsumes('multipart/form-data')
-  @ApiBody({ description: 'Дані для створення картки', type: CreateTeamDto })
-  @ApiResponse({ status: 201, description: 'Картку успішно створено' })
-  @ApiResponse({ status: 400, description: 'Помилка при створенні картки' })
+  @ApiBody({ description: 'Дані для створення члени', type: CreateTeamDto })
+  @ApiResponse({ status: 201, description: 'члена успішно створено' })
+  @ApiResponse({ status: 400, description: 'Помилка при створенні члени' })
   @UseInterceptors(AnyFilesInterceptor())
   async createTeam(@Body() body: any) {
     const parsedBody = typeof body === 'string' ? JSON.parse(body) : body;
@@ -68,13 +68,13 @@ export class TeamsController {
 
   @Put(':id')
   @UseGuards(JwtAuthGuard)
-  @ApiOperation({ summary: 'Оновити картку за ID' })
-  @ApiParam({ name: 'id', required: true, description: 'ID картки' })
+  @ApiOperation({ summary: 'Оновити члена за ID' })
+  @ApiParam({ name: 'id', required: true, description: 'ID члени' })
   @ApiConsumes('multipart/form-data')
-  @ApiBody({ description: 'Дані для оновлення картки', type: UpdateTeamDto })
-  @ApiResponse({ status: 200, description: 'Картку успішно оновлено' })
-  @ApiResponse({ status: 400, description: 'Помилка при оновленні картки' })
-  @ApiResponse({ status: 404, description: 'Картку не знайдено' })
+  @ApiBody({ description: 'Дані для оновлення члени', type: UpdateTeamDto })
+  @ApiResponse({ status: 200, description: 'члена успішно оновлено' })
+  @ApiResponse({ status: 400, description: 'Помилка при оновленні члени' })
+  @ApiResponse({ status: 404, description: 'члена не знайдено' })
   @UseInterceptors(AnyFilesInterceptor())
   async updateTeam(
     @Param('id', ParseIntPipe) id: number,
@@ -90,12 +90,12 @@ export class TeamsController {
 
   @Delete(':id')
   @UseGuards(JwtAuthGuard) 
-  @ApiOperation({ summary: 'Видалити картку за ID' })
-  @ApiParam({ name: 'id', required: true, description: 'ID картки' })
-  @ApiResponse({ status: 200, description: 'Картку успішно видалено' })
-  @ApiResponse({ status: 404, description: 'Картку не знайдено' })
+  @ApiOperation({ summary: 'Видалити члена за ID' })
+  @ApiParam({ name: 'id', required: true, description: 'ID члени' })
+  @ApiResponse({ status: 200, description: 'члена успішно видалено' })
+  @ApiResponse({ status: 404, description: 'члена не знайдено' })
   async deleteTeam(@Param('id', ParseIntPipe) id: number) {
     await this.TeamsService.deleteTeam(id);
-    return { message: 'Картку успішно видалено' };
+    return { message: 'члена успішно видалено' };
   }
 }
