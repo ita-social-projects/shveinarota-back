2b03d9e2d5540cdb8a7cd0c049368e3084285b72
Artem Starikov <96979747+IIbreathII@users.noreply.github.com>
2025-03-28 17:17:32 +0200

    Update create-partner.dto.ts

--- back/src/admin/partners/dto/create-partner.dto.ts
+++ back/src/admin/partners/dto/create-partner.dto.ts
@@ -6,4 +6,9 @@ export class CreatePartnerDto {
   @IsString()
   @IsOptional()
   path: string; // Шлях до файлу
+
+  @ApiPropertyOptional({ description: 'Посилання', example: 'uploads/partners/logo.png' })
+  @IsString()
+  @IsOptional()
+  link: string; // Шлях до файлу
 }
